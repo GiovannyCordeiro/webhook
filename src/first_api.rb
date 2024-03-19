fed16510e2c5cf62daad46f33e1e6e143042ca4e
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-19 20:52:27 -0300

    feat: first api

--- src/first_api.rb
+++ src/first_api.rb
@@ -1,1 +1,14 @@
-puts 'hello project'
+require 'sinatra'
+
+before do
+  request.body.rewind
+  @request_payload = JSON.parse request.body.read
+end
+
+get '/' do
+  puts 'hello world'
+end
+
+post '/recieve_event' do
+  puts "evento que ocorreu no sistema 2, os dados são: #{@request_payload['body']}"
+end
